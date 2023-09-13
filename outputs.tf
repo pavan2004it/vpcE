b4e7e6c0c07c9b9e2980ef0a88fad5d79633d5b8
Pavan Kumar <pavan_2004it@icloud.com>
2023-09-13 15:28:23 +0530

    added security group option

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,17 @@ output "dns_entry" {
 output "hosted_zone" {
   value = [for k in aws_vpc_endpoint.this : k.dns_entry[0]["hosted_zone_id"]]
 }
+
+################################################################################
+# Security Group
+################################################################################
+
+output "security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the security group"
+  value       = try(aws_security_group.this[0].arn, null)
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = try(aws_security_group.this[0].id, null)
+}
