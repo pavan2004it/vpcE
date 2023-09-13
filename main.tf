016be80e981c413a082274b6c4831893f42937da
Pavan Kumar <pavan_2004it@icloud.com>
2023-09-13 15:20:37 +0530

    added security group option

--- main.tf
+++ main.tf
@@ -40,3 +40,45 @@ resource "aws_vpc_endpoint" "this" {
     delete = lookup(var.timeouts, "delete", "10m")
   }
 }
+
+################################################################################
+# Security Group
+################################################################################
+
+resource "aws_security_group" "this" {
+  count = var.create && var.create_security_group ? 1 : 0
+
+  name        = var.security_group_name
+  name_prefix = var.security_group_name_prefix
+  description = var.security_group_description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    var.tags,
+    var.security_group_tags,
+    { "Name" = try(coalesce(var.security_group_name, var.security_group_name_prefix), "") },
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "this" {
+  for_each = { for k, v in var.security_group_rules : k => v if var.create && var.create_security_group }
+
+  # Required
+  security_group_id = aws_security_group.this[0].id
+  protocol          = try(each.value.protocol, "tcp")
+  from_port         = try(each.value.from_port, 443)
+  to_port           = try(each.value.to_port, 443)
+  type              = try(each.value.type, "ingress")
+
+  # Optional
+  description              = try(each.value.description, null)
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
+  prefix_list_ids          = lookup(each.value, "prefix_list_ids", null)
+  self                     = try(each.value.self, null)
+  source_security_group_id = lookup(each.value, "source_security_group_id", null)
+}
